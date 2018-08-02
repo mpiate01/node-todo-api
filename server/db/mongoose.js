c8b2a555070ffb6b36051be284e2d49d76e9c552
Manuel Piatesi <mpiatesi@phase-eight.co.uk>
2018-08-02 08:56:19 +0100

    prova env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+console.log('---------------------------------------------------------------------------------------------', process.env.MONGODB_URI)
 mongoose.Promise = global.Promise
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
 
